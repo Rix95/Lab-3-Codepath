204b5c1f2796c42e1d201338ba95db4ddcc997b0
Rix95 <ricalrax@gmail.com>
2023-10-16 22:57:21 -0500

    add project

---
+++ app/src/main/java/com/codepath/flixster/Movie.kt
@@ -0,0 +1,27 @@
+package com.codepath.flixster
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class Movie {
+    @SerializedName("id")
+    var rank = 0
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+    @JvmField
+    @SerializedName("overview")
+    var overview: String? = null
+
+    @SerializedName("poster_path")
+    var posterUrl: String? = null
+
+
+}
